842c4b2d2839de63b59aad84e2529bf20402f2e4
chenqiuli <sqiulia@163.com>
2023-07-17 22:59:55 +0800

    安全中心页面联调

--- pages/savecenter/savecenter.js
+++ pages/savecenter/savecenter.js
@@ -1,12 +1,116 @@
 // pages/savecenter/savecenter.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    showPwd: false,
+    showZhuxiao: false,
+    showLogout: false,
+    password: "",
+    oldPassword: ""
   },
+
+  // changePassword(event) {
+  //   console.log(event);
+  //   this.setData({
+  //     [event.target.dataset.key]: event.detail
+  //   });
+  // },
+
+  handleUpdatePwd() {
+    wx.request({
+      url: 'http://www.kangliuyong.com:10002/updatePassword',
+      method: 'POST',
+      data: {
+        appkey: "U2FsdGVkX19WSQ59Cg+Fj9jNZPxRC5y0xB1iV06BeNA=",
+        tokenString: wx.getStorageSync('token'),
+        password: this.data.password,
+        oldPassword: this.data.oldPassword,
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+      },
+      success: res => {
+        // console.log(res)
+        if (res.data.code === 'E003') {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'error'
+          });
+        }
+      }
+    })
+  },
+
+  handleEditPwd() {
+    this.setData({
+      showPwd: !this.data.showPwd
+    });
+  },
+
+  handleZhuXiao() {
+    this.setData({
+      showZhuxiao: !this.data.showZhuxiao
+    });
+  },
+
+
+  handleSureZhuxiao() {
+    wx.request({
+      url: 'http://www.kangliuyong.com:10002/destroyAccount',
+      method: 'POST',
+      data: {
+        appkey: "U2FsdGVkX19WSQ59Cg+Fj9jNZPxRC5y0xB1iV06BeNA=",
+        tokenString: wx.getStorageSync('token'),
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+      },
+      success: res => {
+        if(res.data.code === 'G001') {
+          wx.showToast({
+            title: res.data.msg,
+          });
+          wx.removeStorageSync('token');
+        }
+      }
+    })
+  },
+
+  handleLogout() {
+    this.setData({
+      showLogout: !this.data.showLogout
+    });
+  },
+
+  handleSureLogout() {
+    wx.request({
+      url: 'http://www.kangliuyong.com:10002/logout',
+      method: 'POST',
+      data: {
+        appkey: "U2FsdGVkX19WSQ59Cg+Fj9jNZPxRC5y0xB1iV06BeNA=",
+        tokenString: wx.getStorageSync('token'),
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+      },
+      success: res => {
+        if(res.data.code === 'F001') {
+          wx.showToast({
+            title: res.data.msg,
+          });
+          wx.removeStorageSync('token');
+          wx.redirectTo({
+            url: '/pages/login/login',
+          });
+        }
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
